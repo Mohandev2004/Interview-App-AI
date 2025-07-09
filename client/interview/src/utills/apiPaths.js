4a4751df7fc8ee4212196bba569bd94956823f28
Mohan A <the.mohan2004@gmail.com>
2025-07-09 10:27:52 +0530

    Update

--- client/interview/src/utills/apiPaths.js
+++ client/interview/src/utills/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://interview-app-ai-one.vercel.app/";
+export const BASE_URL = "https://interview-app-ai-lemon.vercel.app/api";
 
 export const API_PATHS = {
     AUTH: {
