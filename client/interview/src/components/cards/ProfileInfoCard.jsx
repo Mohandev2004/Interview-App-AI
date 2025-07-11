1e04e3757cc3333a7930c8bb2313132f84d2e6e0
Mohan A <the.mohan2004@gmail.com>
2025-07-12 01:27:25 +0530

    update

--- client/interview/src/components/cards/ProfileInfoCard.jsx
+++ client/interview/src/components/cards/ProfileInfoCard.jsx
@@ -16,16 +16,16 @@ const ProfileInfoCard = () => {
     user && (
       <div className="flex items-center ">
         <div className="w-12 h-12 rounded-full bg-amber-100 text-amber-600 flex items-center justify-center text-xl font-semibold shadow-sm">
-  {user?.profileImage ? (
-    <img
-      src={user.profileImage}
-      alt="Profile"
-      className="w-full h-full rounded-full object-cover"
-    />
-  ) : (
-    user?.name?.charAt(0).toUpperCase() || "U"
-  )}
-</div>
+          {user?.profileImageUrl ? (
+            <img
+              src={user.profileImageUrl}
+              alt="Profile"
+              className="w-full h-full rounded-full object-cover"
+            />
+          ) : (
+            user?.name?.charAt(0).toUpperCase() || "U"
+          )}
+        </div>
 
         <div className='px-3'>
           <div className="text-[15px] text-black font-bold leading-3 ">
